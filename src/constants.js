138b20931a603cb4dace41d485f380811fd4cbf2
Ashborn14 <neon99156@gmail.com>
2024-04-14 16:34:15 +0530

    inital commit

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in Android and React nowadays.
+      I've made a couple of project in Django Framework also. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/AshBorn14" target="_blank">Github</a>!`,
+    "cs-degree": `This is my BCA Degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes i write it down in my notes app in my phone.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/AshBorn14/Resume/blob/main/updated%20resume2.pdf" target="_blank">Check it out?</a>
+    Contact me at shivyadav0714@gmal.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of books on my shelves from different Genres because i like to read books from multipe genres. I don't want to confine myself by reading only one genre`,
+    exit: `If you want to exit Shiv's portfolio, just close the tab.`,
+  };
